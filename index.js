06759d2606b894178cd8ade071aa711c8e02a84c
Harrison <harrisonjones5678@gmail.com>
2021-01-21 22:03:39 -0500

    add script tag, index.js file is a wip, change css selector name

---
+++ index.js
@@ -0,0 +1,9 @@
+const moreLessButton = document.getElementById('more-less-button');
+
+// const handleInfoButtonClick = () => {
+//     moreLessButton.
+// }
+
+console.log('class-list', moreLessButton.classList);
+
+// moreLessButton.addEventListener()
