d62b068082b6bb3077cb52fa063b6e44729d70c1
Harrison <harrisonjones5678@gmail.com>
2021-04-12 21:32:20 -0400

    add more click functionality

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const buttonText = document.getElementById("button-text");
 
 const arrowIcon = document.getElementById("arrow");
 
+const timeZoneDrawer = document.getElementById("time-zone-drawer");
+
+const quoteContainer = document.getElementById("quote-container");
+
+const infoWrapper = document.getElementById("info-wrapper");
+
 const handleInfoButtonClick = () => {
   if (buttonText.innerHTML === "more") {
     buttonText.innerHTML = "less";
     arrowIcon.classList.add("arrow-rotated");
+    timeZoneDrawer.classList.add("expanded");
+    quoteContainer.classList.add("hidden");
+    infoWrapper.classList.remove("hidden");
+    timeZoneDrawer.scrollIntoView();
   } else {
     buttonText.innerHTML = "more";
     arrowIcon.classList.remove("arrow-rotated");
+    timeZoneDrawer.classList.remove("expanded");
+    quoteContainer.classList.remove("hidden");
+    infoWrapper.classList.add("hidden");
   }
 };
 
